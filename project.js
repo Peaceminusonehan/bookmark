a43ae072371a912d9429a2124d9b46de248bc915
Peaceminusonehan <93480834+Peaceminusonehan@users.noreply.github.com>
2021-12-02 10:37:37 +0630

    firstlay

---
+++ project.js
@@ -0,0 +1,23 @@
+const spanlay =document.getElementsByClassName(".span")
+
+const spanlist =[{
+    name:"Simple Bookmarking",
+    id:1
+},
+{
+    name:"Simple Bookmarking",
+    id:2
+},
+{
+    name:"Simple Bookmarking",
+    id:3
+},]
+
+  for (let i = 0; index <spanlist.length; i++) {
+    const spans = document.createElement("span")
+    spans.append(spanlist[i].name)
+   
+    
+    
+}
+console.log("fjdfj")
